7d235b611f45068742e9fafbaae93e2898eb1bba
Ali Abdelbadei <aliabdelbadei325@gmail.com>
2026-06-14 14:48:42 +0300

    Update API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL =
-"https://striking-illumination-production-b911.up.railway.app/api";
+export const API_URL = "https://striking-illumination-production-b911.up.railway.app/api";
+
 // Create axios instance
 const api = axios.create({
     baseURL: API_URL,
